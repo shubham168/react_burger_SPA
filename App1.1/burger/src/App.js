3c8d50581f5a37d6db299f3d3ebaa17be778e204
Shubham Jadhav <jadhavshubham168@gmail.com>
2021-03-30 20:33:34 +0530

    Burge builder component

--- App1.1/burger/src/App.js
+++ App1.1/burger/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Layout from "./Component/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p> Test</p>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
