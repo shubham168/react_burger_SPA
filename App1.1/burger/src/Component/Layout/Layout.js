83d21f55fff6303457f7960a185502b4e5ee4d10
Shubham Jadhav <jadhavshubham168@gmail.com>
2021-04-01 11:55:26 +0530

    Adjusted logo for mobile devices

--- App1.1/burger/src/Component/Layout/Layout.js
+++ App1.1/burger/src/Component/Layout/Layout.js
@@ -2,10 +2,11 @@ import React from "react";
 import Aux from "../../hoc/Auxilary";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
-
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 const layout = (props) => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <div> sideBar</div>
     <main className={classes.Content}>{props.children}</main>
   </Aux>
